5378f4f526ab0028f1aac09e98ae7119bef3eaa7
HenryJohnson26 <108373639+HenryJohnson26@users.noreply.github.com>
2022-07-03 20:11:27 -0600

    added clocktime2

--- clocktime2/clock_time.cpp
+++ clocktime2/clock_time.cpp
@@ -7,7 +7,8 @@
  *
  * The goal of this file is to create the methods used by the clock_time class
  * so that it can add and adjust time with wraparounds as well as methods for
- * for the to_string and convert methods
+ * for the to_string and operation ovverides
+ * extra credit completed
  */
 #include "clock_time.h"
 #include <iostream>
@@ -20,6 +21,12 @@ int clock_time::get_minute() {
 int clock_time::get_second() {
     return second;
 }
+int clock_time::set_twelve(int h){
+    if(h>12)h-=12;
+    return h;
+}
+
+
 void clock_time::set_time(int h, int m, int s) {
     //multiplying to get total seconds
     second = (h*60*60)+(m*60)+(s);
@@ -30,21 +37,50 @@ void clock_time::set_time(int h, int m, int s) {
     minute = (second/60)%60;
     second = second%60;
 }
-string clock_time::pad_number(int number) {
+void clock_time::set_now() {
+    time_t clock1;
+    struct tm * timeinfo;
+    time (&clock1);
+    timeinfo = localtime (&clock1);
+    set_time(timeinfo->tm_hour,timeinfo->tm_min,timeinfo->tm_sec);
+    cout<<"current time set to time set to: "<<timeinfo->tm_hour<<":"<<timeinfo->tm_min<<":"<<timeinfo->tm_sec<<endl;
+}
+
+
+string clock_time::pad_number(int number) const {
     string out;
     if(number<10) out= "0" + std::to_string(number);
     else out = std::to_string(number);
     return out;
 }
-string clock_time::to_string(clock_time c) {
+string to_string(const clock_time& c) {
     string min = c.pad_number(c.minute);
     string sec = c.pad_number(c.second);
     string hour = std::to_string(c.hour);
     return hour+":"+min+":"+sec;
 }
-clock_time clock_time::convert(string time) {
-
+string to_string(clock_time& c, const bool twelveHour){
+    string min = c.pad_number(c.minute);
+    string sec = c.pad_number(c.second);
+    int hour = c.set_twelve(c.hour);
+    return to_string(hour)+":"+min+":"+sec;
 }
-string clock_time::to_string(string time) {
-    convert(time);
+
+
+ostream& operator<<(ostream& out, const clock_time& c){
+    out<<to_string(c);
+    return out;
+}
+bool operator==(const clock_time& first, const clock_time& second){
+    return first.hour == second.hour && first.minute == second.minute && first.second == second.second;
+}
+bool operator!=(const clock_time& first, const clock_time& second){
+    return !(first == second);
+}
+clock_time operator+(const clock_time& first, const clock_time& second){
+    clock_time out;
+    out.set_time(first.hour + second.hour,
+                 first.minute + second.minute,
+                 first.second + second.second);
+    return out;
 }
--- clocktime2/clock_time.h
+++ clocktime2/clock_time.h
@@ -7,15 +7,23 @@
  *
  * the goal of this class is to create headers and instance data in order add and adjust
  * time with wrapping, as well as headers for the to_string and convert methods.
- * it will also be used to overide the << operator
+ * it will also initialize and friend the operator ovveriders
+ * extra credit completed
  */
 
 #ifndef CLOCKTIME_CLOCK_TIME_H
 #define CLOCKTIME_CLOCK_TIME_H
 #include <iostream>
+#include <sstream>
+#include <ctime>
 using namespace std;
 class clock_time{
-    friend ostream & operator<<(ostream & out, clock_time c);
+    friend ostream& operator<<(ostream& out, const clock_time& c);
+    friend string to_string(const clock_time& c);
+    friend string to_string(clock_time& c, bool twelveHour);
+    friend bool operator==(const clock_time& first, const clock_time& second);
+    friend bool operator!=(const clock_time& first, const clock_time& second);
+    friend clock_time operator+(const clock_time& first, const clock_time& second);
     //Data to be used by the clock_time class privated so to not be accesible by main
 private:
     int hour;
@@ -27,11 +35,15 @@ public:
 int get_hour();
 int get_minute();
 int get_second();
+int set_twelve(int h);
+
 void set_time(int h, int m, int s);
-string pad_number(int number);
-string to_string(clock_time c);
-string to_string(string time);
-clock_time convert(string time);
+void set_now();
+
+string pad_number(int number) const;
+
+//string to_string(clock_time& c);
+//ostream& operator<<(ostream& out, clock_time& c);
 //constructors of the clock_time class
 clock_time(){
         set_time(0,0,0);
@@ -42,9 +54,9 @@ clock_time(int h, int m, int s){
 //destructor of the clock_time class
 ~clock_time() {}
 
+
+
 };
-ostream & operator<<(ostream & out, clock_time c){
-    out<<c.to_string(c);
-    return out;
-}
+
+
 #endif //CLOCKTIME_CLOCK_TIME_H
